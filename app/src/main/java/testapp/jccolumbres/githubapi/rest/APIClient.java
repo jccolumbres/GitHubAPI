67f36373d1c4f67f8072e8001290e0e53f49fdd5
Jigs <jhegs.knightdev@gmail.com>
2018-09-26 15:42:47 +0800

    Added new retrofit declaration

--- app/src/main/java/testapp/jccolumbres/githubapi/rest/APIClient.java
+++ app/src/main/java/testapp/jccolumbres/githubapi/rest/APIClient.java
@@ -17,4 +17,17 @@ public class APIClient {
         }
         return retrofit;
     }
+
+    /* Alternative declaration for Retrofit (No OkHttp)
+    private static final String BASE_URL = "http://10.0.2.2:9000/";
+    private static Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = builder.build();
+
+    public static <S> S buildService(Class<S> serviceType){
+        return retrofit.create(serviceType);
+    }
+     */
 }
